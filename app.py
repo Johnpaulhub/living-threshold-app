bbd84b6c36da7e29d24fa1788816bca863047616
John Paul Omondi <168242773+Johnpaulhub@users.noreply.github.com>
2026-09-18 23:54:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ def on_get_residues(data):
             })
 
     emit("residues", results)
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host="0.0.0.0", port=port, debug=False)
+
