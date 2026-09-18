24fe0c74cc76a7b60fadea1fe329d734e25deff6
John Paul Omondi <168242773+Johnpaulhub@users.noreply.github.com>
2026-09-19 00:03:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -210,7 +210,8 @@ def on_get_residues(data):
             })
 
     emit("residues", results)
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=False)
-
+    # allow_unsafe_werkzeug=True added to fix the production deployment crash
+    socketio.run(app, host="0.0.0.0", port=port, debug=False, allow_unsafe_werkzeug=True)
